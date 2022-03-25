cf1aa4bb9524cd02f107125a2fbd48ea94938397
Firdaousse <firda@FMOUSSA>
2022-03-25 12:16:20 +0100

    Debut compte

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Module } from '../models/module.model';
 import { Chapitre } from '../models/chapitre.model';
 import { Programme } from '../models/programme.model';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,33 +17,60 @@ export class DataService {
 
   constructor(private httpClient: HttpClient) { }
 
-  /** PROGRAMMEs **/
-  
-  // get all modules
+
+
+
+  /** ACCOUNT **/
+
+  // inscription
+  inscription(donneeUser: User) {
+    return this.httpClient.post(this.adresse + 'register', donneeUser);
+  }
+
+  // connexion
+  connexion(donneeUser: User) {
+    return this.httpClient.post(this.adresse + 'login', donneeUser);
+  }
+
+  // déconnexion
+  deconnexion(donneeUser: User) {
+    return this.httpClient.post(this.adresse + 'logout', donneeUser);
+  }
+
+  // get account infos
+  getAccount() {
+    return this.httpClient.get(this.adresse + 'account');
+  }
+
+
+  /** PROGRAMME **/
+
+  // get all programmes
   getAllProgrammesData() {
     return this.httpClient.get(this.adresse + 'programmes');
   }
 
-  
-  // get 1 module par ID
+
+  // get 1 programme par ID
   getProgrammeById(idProg: number) {
     return this.httpClient.get(this.adresse + 'programmes/' + idProg);
   }
 
-  // create module
+  // create programme
   createProgramme(donneeProgrammes: Programme) {
     return this.httpClient.post(this.adresse + 'programmes/create', donneeProgrammes);
   }
 
-  // update module
+  // update programme
   updateProgramme(donneeProgrammes: Programme, idProg: number) {
     return this.httpClient.put(this.adresse + 'programmes/' + idProg + '/edit', donneeProgrammes);
   }
 
-  // delete module
+  // delete programme
   deleteProgramme(idProg: number) {
     return this.httpClient.delete(this.adresse + 'programmes/' + idProg + '/edit');
   }
+
 
   /** MODULE **/
 
@@ -70,6 +98,7 @@ export class DataService {
     return this.httpClient.delete(this.adresse + 'modules/' + idMod + '/edit');
   }
 
+
   /** CHAPITRE **/
 
   // get all chapitres
@@ -85,7 +114,7 @@ export class DataService {
     return this.httpClient.post(this.adresse + 'modules/' + idMod + '/edit/content/create', donneeChapitre);
   }
   // update chapitre
-  updateChapitre(donneeChapitre: Chapitre, idMod: number, idChap: number) { 
+  updateChapitre(donneeChapitre: Chapitre, idMod: number, idChap: number) {
     return this.httpClient.put(this.adresse + 'modules/' + idMod + '/edit/content/' + idChap, donneeChapitre);
   }
   // delete chapitre
@@ -96,5 +125,5 @@ export class DataService {
   getOrdreDoc(idMod: number) {
     return this.httpClient.get(this.adresse + 'modules/' + idMod + '/edit/content/create');
   }
-  
+
 }
